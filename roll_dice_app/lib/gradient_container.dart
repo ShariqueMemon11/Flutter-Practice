1d133e6f2831f83af47c4a2561114f1dc0719c94
Shariq.x <memonshariq10@gmail.com>
2025-02-20 19:12:18 +0500

    adding buttons

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:roll_dice_app/style_text.dart';
 
 class GradientContainer extends StatelessWidget {
   const GradientContainer(this.color1, this.color2, {super.key});
+
   final Color color1;
   final Color color2;
+
   @override
-  Widget build(context) {
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
         gradient: LinearGradient(
@@ -15,7 +16,30 @@ class GradientContainer extends StatelessWidget {
           end: Alignment.bottomCenter,
         ),
       ),
-      child: Center(child: Style_text("hello shariq!")),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset('assets/dice-2.png', width: 200),
+                Image.asset('assets/dice-2.png', width: 200),
+              ],
+            ),
+            const SizedBox(height: 40), // Adds spacing
+            TextButton(
+              onPressed: () {
+                print("Button Pressed!");
+              },
+              child: const Text(
+                "Roll Dice",
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
